aa57402d72b133c5f1cc271328a4bf7e22c34893
Ibrahim Hz <ibrahimdmc666@gmail.com>
2023-05-24 14:11:16 +0530

    create device page

--- frontend/src/pages/DashBoard/DashboardPage.jsx
+++ frontend/src/pages/DashBoard/DashboardPage.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
-
+import {Link} from "react-router-dom"
 const DashboardPage = () => {
   return (
-    <div>DashboardPage</div>
+    <div>
+    DashboardPage
+    <h1> <Link to="/devices"> Goto Devices Page here</Link></h1>
+    </div>
   )
 }
 
